da27faee20b3d48241f88eda7d7da724deb82538
Soubik <soubik.bhui16@gmail.com>
2021-05-26 11:14:04 +0530

    server.go

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-package serve
+package main
 
 import (
 	"fmt"
@@ -7,7 +7,22 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+
+	"github.com/urfave/cli/v2"
 )
+
+var version = "0.0.1"
+
+type Options struct {
+	// Port Number (default=8080)
+	Port int
+
+	// Path of custom 404.html file [only file path]
+	Error404 string
+
+	// Slice of all the paths to ignore, if any request is made to access the ignored file(s), it will be redirected to 404.html [only file path(s)]
+	IgnorePath []string
+}
 
 var Opts Options
 
@@ -90,4 +105,67 @@ func Start() {
 	http.Handle("/", fileServerC(http.Dir(fp)))
 	serv := fmt.Sprintf(":%v", Opts.Port)
     fmt.Print(http.ListenAndServe(serv, nil))
+}
+
+func main() {
+	(&cli.App{
+		Name: "gnet",
+		Version: version,
+		Description: "Simple Web Server in Go",
+		Usage: "Simple Web Server in Go",
+		UsageText: "gnet [command] [opts...] [args...]",
+		CustomAppHelpTemplate: fmt.Sprintf(`Simple Web Server in Go
+
+VERSION: %v
+
+COMMANDS:
+		start	Start the server
+		help, h Shows a list of commands or help for one command
+
+SERVER OPTIONS:
+		--port		Give the port number (default=8080)
+		--404		Set a custom 404 error page
+		--ignore		Set a list of files/folders to ignore, this will load 404.html if any request is made
+
+GLOBAL OPTIONS:
+		--help, -h	show help
+		--version, -v	print the version
+`, version),
+		Commands: []*cli.Command{
+			{
+				Name: "start",
+				Description: "Start a server",
+				Usage: "Start a server",
+				UsageText: "start [opts...] [args...]",
+				Action: func(c *cli.Context) error {
+					Opts = Options{
+						Port: c.Int("port"),
+						Error404: c.String("404"),
+						IgnorePath: c.StringSlice("ignore"),
+					}
+					Start()
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.IntFlag{
+						Name: "port",
+						Usage: "Port Number",
+						Value: 8080,
+						Required: false,
+					},
+					&cli.StringFlag{
+						Name: "404",
+						Usage: "Set custom 404.html",
+						Value: "",
+						Required: false,
+					},
+					&cli.StringSliceFlag{
+						Name: "ignore",
+						Usage: "Set the folder and file paths to ignore. This will load 404.html if any request if made",
+						Required: false,
+					},
+				},
+			},
+		},
+	}).Run(os.Args)
 }
